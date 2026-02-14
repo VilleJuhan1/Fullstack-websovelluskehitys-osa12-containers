30c1361868996c51139f4702fc81bc28038a94e8
Ville-Juhani Nivasalo <ville.nivasalo@gmail.com>
2026-02-14 09:51:15 +0200

    mongo working locally

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -35,12 +35,16 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo);
 });
 
 /* PUT todo. */
-singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+singleRouter.put('/', async (_, res) => {
+  const { text, done } = _.body
+  req.todo.text = text
+  req.todo.done = done
+  await req.todo.save()
+  res.send(req.todo);
 });
 
 router.use('/:id', findByIdMiddleware, singleRouter)
