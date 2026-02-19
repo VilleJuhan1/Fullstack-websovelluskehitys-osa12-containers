e5ed4e778814c7f81641900f1d593a17184c4e39
Ville-Juhani Nivasalo <ville.nivasalo@gmail.com>
2026-02-19 17:55:38 +0200

    refactored component

--- todo-app/todo-frontend/src/Todos/List.jsx
+++ todo-app/todo-frontend/src/Todos/List.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Todo from './Todo'
 
 const TodoList = ({ todos, deleteTodo, completeTodo }) => {
   const onClickDelete = (todo) => () => {
@@ -10,39 +11,28 @@ const TodoList = ({ todos, deleteTodo, completeTodo }) => {
   }
 
   return (
-    <>
-      {todos.map(todo => {
-        const doneInfo = (
-          <>
-            <span>This todo is done</span>
-            <span>
-              <button onClick={onClickDelete(todo)}> Delete </button>
-            </span>
-          </>
-        )
-
-        const notDoneInfo = (
-          <>
-            <span>
-              This todo is not done
-            </span>
-            <span>
-              <button onClick={onClickDelete(todo)}> Delete </button>
-              <button onClick={onClickComplete(todo)}> Set as done </button>
-            </span>
-          </>
-        )
-
-        return (
-          <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '70%', margin: 'auto' }}>
-            <span>
-              {todo.text} 
-            </span>
-            {todo.done ? doneInfo : notDoneInfo}
-          </div>
-        )
-      }).reduce((acc, cur) => [...acc, <hr />, cur], [])}
-    </>
+    <div style={{ marginTop: '1rem' }}>
+      {todos.flatMap((todo, i) =>
+        i === 0
+          ? [
+              <Todo
+                key={todo._id}
+                todo={todo}
+                onDelete={onClickDelete(todo)}
+                onComplete={onClickComplete(todo)}
+              />
+            ]
+          : [
+              <hr key={`sep-${todo._id}`} />,
+              <Todo
+                key={todo._id}
+                todo={todo}
+                onDelete={onClickDelete(todo)}
+                onComplete={onClickComplete(todo)}
+              />
+            ]
+      )}
+    </div>
   )
 }
 
